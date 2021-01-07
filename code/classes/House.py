675fce0af60d6554d72abfdb721c0327cfc8f3cc
Shu Yan Ng <shuyan@MacBook-Pro-van-Shu.local>
2021-01-07 19:16:33 +0100

    breedte en diepte omgewisseld

--- code/classes/House.py
+++ code/classes/House.py
@@ -18,14 +18,14 @@ class House():
             self.value = 285000
             self.free_area = 2
         elif self.house_type == 2:
-            self.length = 7
-            self.width = 11
+            self.length = 11
+            self.width = 7
             self.increase_value = 0.04
             self.value = 399000
             self.free_area = 3
         elif self.house_type == 3:
-            self.length = 10
-            self.width = 12
+            self.length = 12
+            self.width = 10
             self.increase_value = 0.06
             self.value = 610000
             self.free_area = 6
@@ -37,15 +37,15 @@ class House():
             check = True
         
         # check corner 2 (bottom right)
-        elif self.x_top_right + self.free_area == 180 or self.y_bottom_left - self.free_area == 0:
+        elif self.x_top_right + self.free_area == 160 or self.y_bottom_left - self.free_area == 0:
             check = True
         
         # check corner 3 (top right)
-        elif self.x_top_right + self.free_area == 180 or self.y_top_right + self.free_area == 160:
+        elif self.x_top_right + self.free_area == 160 or self.y_top_right + self.free_area == 180:
             check = True
 
         # check corner 4 (top left)
-        elif self.x_bottom_left - self.free_area == 0 or self.y_top_right + self.free_area == 160:
+        elif self.x_bottom_left - self.free_area == 0 or self.y_top_right + self.free_area == 180:
             check = True
 
         return check
